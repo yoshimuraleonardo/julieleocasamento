38025d933818f429be852a3477944c69c1f33d5d
yoshimuraleonardo <yoshimura.leonardo@gmail.com>
2026-09-18 12:51:34 -0300

    Update gifts.js

--- gifts.js
+++ gifts.js
@@ -37,7 +37,7 @@ const GIFTS = [
     description: "Mergulho em Maraú - BA",
     price: "R$ 150",
     amount: 150,
-    pixLink: https://nubank.com.br/cobrar/f619r/6aad4fed-b3f6-4043-9377-03c16921b72a,
+    pixLink: "https://nubank.com.br/cobrar/f619r/6aad4fed-b3f6-4043-9377-03c16921b72a",
     image: "images/Gemini_Generated_Image_pynqocpynqocpynq.jpg",
   },
   {
@@ -45,7 +45,7 @@ const GIFTS = [
     description: "Uma tarde de drinks no melhor por do sol de Maraú",
     price: "R$ 200",
     amount: 200,
-    pixLink: https://nubank.com.br/cobrar/f619r/6aad503f-a5a9-4cba-bbbd-c41cb6051d37,
+    pixLink: "https://nubank.com.br/cobrar/f619r/6aad503f-a5a9-4cba-bbbd-c41cb6051d37",
     image: "images/Gemini_Generated_Image_pynqocpynqocpynq (7).jpg",
   },
   {
@@ -53,7 +53,7 @@ const GIFTS = [
     description: "Um típico almoço Baiano",
     price: "R$ 250",
     amount: 250,
-    pixLink: https://nubank.com.br/cobrar/f619r/6aad5068-a00e-44c5-a467-6672fb738e3d,
+    pixLink: "https://nubank.com.br/cobrar/f619r/6aad5068-a00e-44c5-a467-6672fb738e3d",
     image: "images/Gemini_Generated_Image_pynqocpynqocpynq (6).jpg",
   },
    {
@@ -61,7 +61,7 @@ const GIFTS = [
     description: "Passeio de barco pelas praias da Região",
     price: "R$ 275",
     amount: 275,
-    pixLink: https://nubank.com.br/cobrar/f619r/6aad50c2-9579-42f3-b74f-67918229419a,
+    pixLink: "https://nubank.com.br/cobrar/f619r/6aad50c2-9579-42f3-b74f-67918229419a",
     image: "images/watermarked_img_7428575731182869263.jpg",
   },
    {
@@ -69,7 +69,7 @@ const GIFTS = [
     description: "Massagem relaxante no Spa do hotel Dreamland",
     price: "R$ 350",
     amount: 350,
-    pixLink: https://nubank.com.br/cobrar/f619r/6aad512e-77f3-4551-b7e3-f7f8157d3e8d,
+    pixLink: "https://nubank.com.br/cobrar/f619r/6aad512e-77f3-4551-b7e3-f7f8157d3e8d",
     image: "images/Gemini_Generated_Image_pynqocpynqocpynq (4).jpg",
   },
   {
@@ -77,7 +77,7 @@ const GIFTS = [
     description: "Aluguel para passeio de quadriciclo",
     price: "R$ 400",
     amount: 400,
-    pixLink: https://nubank.com.br/cobrar/f619r/6aad50e7-c979-4fe4-b379-da266c3fe7be,
+    pixLink: "https://nubank.com.br/cobrar/f619r/6aad50e7-c979-4fe4-b379-da266c3fe7be",
     image: "images/Gemini_Generated_Image_pynqocpynqocpynq (5).jpg",
   },
   {
@@ -85,7 +85,7 @@ const GIFTS = [
     description: "Passeio com ingresso para as cachoeiras de Tremembé",
     price: "R$ 450",
     amount: 450,
-    pixLink: https://nubank.com.br/cobrar/f619r/6aad5157-4dd7-43bb-8193-4db330aa94e5,
+    pixLink: "https://nubank.com.br/cobrar/f619r/6aad5157-4dd7-43bb-8193-4db330aa94e5",
     image: "images/Gemini_Generated_Image_pynqocpynqocpynq (3).jpg",
   },
   {
@@ -93,7 +93,7 @@ const GIFTS = [
     description: "Um jantar romântico completo para celebrarmos em Barra Grande",
     price: "R$ 500",
     amount: 500,
-    pixLink: https://nubank.com.br/cobrar/f619r/6aad5179-1aa3-422d-94fd-1015834f4c71,
+    pixLink: "https://nubank.com.br/cobrar/f619r/6aad5179-1aa3-422d-94fd-1015834f4c71",
     image: "images/535d8ab5-39f6-48d1-9913-259a8e04d559.jpg",
   },
   {
@@ -101,7 +101,7 @@ const GIFTS = [
     description: "Um passeio só nosso de lancha pelas ilhas da região",
     price: "R$ 700",
     amount: 700,
-    pixLink: https://nubank.com.br/cobrar/f619r/6aad5194-0589-48f7-abe1-fa4e89de785c,
+    pixLink: "https://nubank.com.br/cobrar/f619r/6aad5194-0589-48f7-abe1-fa4e89de785c",
     image: "images/Gemini_Generated_Image_pynqocpynqocpynq (2).jpg",
   },
  
